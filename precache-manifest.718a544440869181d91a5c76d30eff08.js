cea17e2859e47197edcc3e48b7e25b564251a704
Maksym Diachenko <max97292@gmail.com>
2021-11-29 15:12:57 +0200

    Updates

---
+++ precache-manifest.718a544440869181d91a5c76d30eff08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a93ab3fd009e54d9750bb880da2bbbd",
+    "url": "/rock-paper-scissors-game/index.html"
+  },
+  {
+    "revision": "9b640dc6122f94ef2120",
+    "url": "/rock-paper-scissors-game/static/css/main.72505ed1.chunk.css"
+  },
+  {
+    "revision": "d06aa85e6cde01125914",
+    "url": "/rock-paper-scissors-game/static/js/2.218f4da8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/rock-paper-scissors-game/static/js/2.218f4da8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b640dc6122f94ef2120",
+    "url": "/rock-paper-scissors-game/static/js/main.82ee5a08.chunk.js"
+  },
+  {
+    "revision": "6afb7cf0c842e843b1a7",
+    "url": "/rock-paper-scissors-game/static/js/runtime-main.f4391ec9.js"
+  }
+]);
